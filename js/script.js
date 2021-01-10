b7baea23165630925c070305709a541f6497bac6
Marco.seb <marco.seba96@gmail.com>
2021-01-10 18:03:29 +0100

    Upgrade-bonus

--- js/script.js
+++ js/script.js
@@ -12,11 +12,13 @@ $(document).ready(function(){
         }
     });
 
-    $('.nav i').click(function(){
+    $('.circle i').click(function(){
         $('.active').removeClass('active');
         $(this).addClass('active');
-        $('img').eq($(this).index()).addClass('active');
+        $('.images img').eq($(this).index()).addClass('active');
+        $('h4').eq($(this).index()).addClass('active');
     });
+
 });
 
 
@@ -27,21 +29,25 @@ $(document).ready(function(){
 function slideToRight(){
     //QUI MI SALVO LO STATO DELL'IMMAGINE E DEL PALLINO CON LA CLASSE ACTIVE
     var imgSavedClass = $('.images img.active');
-    var circleActive = $('.nav i.active');
+    var circleActive = $('.circle i.active');
+    var titleActive = $('.title h4.active');
 
     //QUI RIMUOVO LA CLASSE ACTIVE 
     imgSavedClass.removeClass('active');
     circleActive.removeClass('active');
+    titleActive.removeClass('active');
 
     //QUI CONTROLLO L'ELEMENTO LAST E SELZIONO IL PRIMO
-    if(imgSavedClass.hasClass('last')){
+    if(imgSavedClass.hasClass('last') || circleActive.hasClass('last') || titleActive.hasClass('last')){
         $('.images img.first').addClass('active');
-        $('.nav i.first').addClass('active');
+        $('.circle i.first').addClass('active');
+        $('.title h4.first').addClass('active');
     } 
     //QUI SELEZIONO L'ELEMENTO SUCCESSIVO
     else{
         imgSavedClass.next().addClass('active');
         circleActive.next().addClass('active');
+        titleActive.next().addClass('active');
     }
 
 }
@@ -50,26 +56,28 @@ function slideToRight(){
 function slideToLeft(){
     //QUI MI SALVO LO STATO DELL'IMMAGINE E DEL PALLINO CON LA CLASSE ACTIVE
     var imgSavedClass = $('.images img.active');
-    var circleActive = $('.nav i.active');
+    var circleActive = $('.circle i.active');
+    var titleActive = $('.title h4.active');
+
 
     //QUI RIMUOVO LA CLASSE ACTIVE 
     imgSavedClass.removeClass('active');
     circleActive.removeClass('active');
+    titleActive.removeClass('active');
 
     //QUI CONTROLLO L'ELEMENTO FIRST E SELZIONO L'ULTIMO
-    if(imgSavedClass.hasClass('first')){
+    if(imgSavedClass.hasClass('first') || circleActive.hasClass('first') || titleActive.hasClass('first')){
         $('.images img.last').addClass('active');
-        $('.nav i.last').addClass('active');
+        $('.circle i.last').addClass('active');
+        $('.title h4.last').addClass('active');
+
     } 
     //QUI SELEZIONO L'ELEMENTO SUCCESSIVO
     else{
         imgSavedClass.prev().addClass('active');
         circleActive.prev().addClass('active');
+        titleActive.prev().addClass('active');
     }
 
 }
-
-
-
-
 
